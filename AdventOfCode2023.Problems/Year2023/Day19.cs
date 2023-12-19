808a90ac4ca0d26d4231593b66549cfb81df1017
Michael Kosler <michaelkosler@gmail.com>
2023-12-19 00:41:12 -0800

    Day 19.1

---
+++ AdventOfCode2023.Problems/Year2023/Day19.cs
@@ -0,0 +1,106 @@
+
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Problems.Year2023;
+
+public class Day19 : IProblem
+{
+  private static IDictionary<string, ParameterExpression> PARAMS = new Dictionary<string, ParameterExpression>
+  {
+    ["x"] = Expression.Parameter(typeof(int), "x"),
+    ["m"] = Expression.Parameter(typeof(int), "m"),
+    ["a"] = Expression.Parameter(typeof(int), "a"),
+    ["s"] = Expression.Parameter(typeof(int), "s"),
+  };
+
+  public string Part1(IEnumerable<string> input)
+  {
+    var split = string.Join(Environment.NewLine, input).Split("\r\n\r\n");
+    var workflows = GenerateWorkflows(split[0].Split(Environment.NewLine).Where(l => !string.IsNullOrEmpty(l)));
+
+    var sum = 0;
+
+    foreach (var obj in split[1].Split(Environment.NewLine).Where(l => !string.IsNullOrEmpty(l)))
+    {
+      var match = Regex.Match(obj, @"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}");
+      var x = int.Parse(match.Groups[1].Value);
+      var m = int.Parse(match.Groups[2].Value);
+      var a = int.Parse(match.Groups[3].Value);
+      var s = int.Parse(match.Groups[4].Value);
+
+      Expression wf = workflows["in"];
+
+      while (true)
+      {
+        var lambda = Expression.Lambda<Func<int, int, int, int, string>>(wf, PARAMS.Values);
+        var val = lambda.Compile().Invoke(x, m, a, s);
+
+        if (val == "A")
+        {
+          sum += x + m + a + s;
+          break;
+        }
+        else if (val == "R")
+        {
+          break;
+        }
+
+        wf = workflows[val];
+      }
+    }
+  
+    return $"{sum}";
+  }
+
+  public string Part2(IEnumerable<string> input)
+  {
+    throw new NotImplementedException();
+  }
+
+  private static IDictionary<string, Expression> GenerateWorkflows(IEnumerable<string> input)
+  {
+    var workflows = new Dictionary<string, Expression>();
+
+    foreach (var workflow in input)
+    {
+      var match = Regex.Match(workflow, @"(.+){(.*)}");
+      var name = match.Groups[1].Value;
+      workflows.Add(name, GenerateExpressionFromString(match.Groups[2].Value));
+    }
+
+    return workflows;
+  }
+
+  private static Expression GenerateExpressionFromString(string s)
+  {
+    var exprs = new Stack<(Expression Condition, Expression IfTrue)>();
+    var returnLabel = Expression.Label(typeof(string));
+
+    var pieces = s.Split(",");
+
+    foreach (var p in pieces[..^1])
+    {
+      var match = Regex.Match(p, @"(\w+)([><])(\d+):(\w+)");
+
+      var left = PARAMS[match.Groups[1].Value];
+      // var left = Expression.Parameter(typeof(int), match.Groups[1].Value);
+      var right = Expression.Constant(int.Parse(match.Groups[3].Value));
+      var condition = match.Groups[2].Value switch
+      {
+        ">" => Expression.GreaterThan(left, right),
+        "<" => Expression.LessThan(left, right),
+        _ => throw new Exception("Unknown parameter")
+      };
+
+      exprs.Push((condition, Expression.Constant(match.Groups[4].Value)));
+    }
+
+    Expression current = Expression.Return(returnLabel, Expression.Constant(pieces.Last()));
+
+    // Maybe runs into reference nonsense?
+    while (exprs.TryPop(out var result)) current = Expression.IfThenElse(result.Condition, Expression.Return(returnLabel, result.IfTrue), current);
+
+    return Expression.Block(current, Expression.Label(returnLabel, Expression.Constant("")));
+  }
+}
---
+++ AdventOfCode2023.Test/Year2023Day19.cs
@@ -0,0 +1,57 @@
+namespace AdventOfCode2023.Test;
+
+[TestFixture, TestOf(typeof(Problems.Year2023.Day19))]
+public class Year2023Day19 : AdventOfCodeTestBase<Problems.Year2023.Day19>
+{
+  public Year2023Day19() : base(2023, 19)
+  {
+  }
+
+  [TestCase("px{a<2006:qkq,m>2090:A,rfg}\r\npv{a>1716:R,A}\r\nlnx{m>1548:A,A}\r\nrfg{s<537:gd,x>2440:R,A}\r\nqs{s>3448:A,lnx}\r\nqkq{x<1416:A,crn}\r\ncrn{x>2662:A,R}\r\nin{s<1351:px,qqz}\r\nqqz{s>2770:qs,m<1801:hdj,R}\r\ngd{a>3333:R,R}\r\nhdj{m>838:A,pv}\r\n\r\n{x=787,m=2655,a=1222,s=2876}\r\n{x=1679,m=44,a=2067,s=496}\r\n{x=2036,m=264,a=79,s=2244}\r\n{x=2461,m=1339,a=466,s=291}\r\n{x=2127,m=1623,a=2188,s=1013}", 19114)]
+  [Test, TestOf(nameof(Problems.Year2023.Day19.Part1))]
+  public void Year2023Day19_Part1_Examples(string input, int expected)
+  {
+    // Arrange
+    var lines = input.Split(Environment.NewLine);
+
+    // Act
+    var result = int.Parse(Problem?.Part1(lines) ?? throw new NullReferenceException());
+
+    // Assert
+    Assert.That(result, Is.EqualTo(expected));
+  }
+
+  [Test, TestOf(nameof(Problems.Year2023.Day19.Part1))]
+  public void Year2023Day19_Part1_Solution()
+  {
+    // Act
+    var result = int.Parse(Problem?.Part1(Input) ?? throw new NullReferenceException());
+
+    // Assert
+    Assert.That(result, Is.EqualTo(446517));
+  }
+
+  [TestCase("px{a<2006:qkq,m>2090:A,rfg}\r\npv{a>1716:R,A}\r\nlnx{m>1548:A,A}\r\nrfg{s<537:gd,x>2440:R,A}\r\nqs{s>3448:A,lnx}\r\nqkq{x<1416:A,crn}\r\ncrn{x>2662:A,R}\r\nin{s<1351:px,qqz}\r\nqqz{s>2770:qs,m<1801:hdj,R}\r\ngd{a>3333:R,R}\r\nhdj{m>838:A,pv}\r\n\r\n{x=787,m=2655,a=1222,s=2876}\r\n{x=1679,m=44,a=2067,s=496}\r\n{x=2036,m=264,a=79,s=2244}\r\n{x=2461,m=1339,a=466,s=291}\r\n{x=2127,m=1623,a=2188,s=1013}", 19114)]
+  [Test, TestOf(nameof(Problems.Year2023.Day19.Part2))]
+  public void Year2023Day19_Part2_Examples(string input, long expected)
+  {
+    // Arrange
+    var lines = input.Split(Environment.NewLine);
+
+    // Act
+    var result = long.Parse(Problem?.Part2(lines) ?? throw new NullReferenceException());
+
+    // Assert
+    Assert.That(result, Is.EqualTo(expected));
+  }
+
+  [Test, TestOf(nameof(Problems.Year2023.Day19.Part2))]
+  public void Year2023Day19_Part2_Solution()
+  {
+    // Act
+    var result = long.Parse(Problem?.Part2(Input) ?? throw new NullReferenceException());
+
+    // Assert
+    Assert.That(result, Is.EqualTo(131431655002266));
+  }
+}
